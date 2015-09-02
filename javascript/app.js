545983cc9af7718a0ed0bbf43f5440f70c9db347
Stephen Florian <steezeven@gmail.com>
2015-09-02 11:55:30 -0600

    added ai. turn counter bug still needs resolved.

--- javascript/app.js
+++ javascript/app.js
@@ -9,20 +9,25 @@ $(document).ready(function(){
   var shaka = '<img src="images/shaka.png">';
   var bro = '<img src="images/bro.png">';
   var positions = ['left', 'top', 'right', 'horizontal', 'vertical', 'diagonal-left', 'diagonal-right'];
+  var timer;
+  var playingAgainstComputer = true;
 
-  function myTurnBro(teamName, movesArray, score, className, that) {
-    $(that).html(teamName);
+  function myTurnBro(shakaOrBro, movesArray, scoreClass, myTurnClass, theirTurnClass, that) {
+    stopTimer();
+    $(that).html(shakaOrBro);
     newMove = $(that).attr('data').split(' ');
     $.merge(movesArray, newMove);
     if (hasThreeInARow(movesArray)) {
-      score += 1;
-      console.log(score, broScore, shakaScore)
-      $(className).html(score);
+      var score = Number($(scoreClass).html());
+      $(scoreClass).html(score + 1);
       reset();
-      turnCounter += 1;
+      turnCounter++;
     }
-    endOfGameCounter += 1
-    turnCounter += 1;
+    $(theirTurnClass).slideDown('fast');
+    $(myTurnClass).css('display', 'none');
+    endOfGameCounter++;
+    turnCounter++;
+    switchTurnsCountdown();
   }
 
   function hasThreeInARow(playerMoves){
@@ -30,7 +35,6 @@ $(document).ready(function(){
       obj[val] = obj[val] + 1 || 1;
       return obj;
     },{});
-    console.log(recurrences);
     for (anyMove in recurrences){
       if (recurrences[anyMove] === 3) {
         return true;
@@ -47,6 +51,37 @@ $(document).ready(function(){
     $('.grid').removeClass('clicked');
   }
 
+  function switchTurnsCountdown(){
+    function switchTurn(myTurnClass, theirTurnClass){
+      timer = setTimeout(function(){
+        turnCounter++;
+        $(theirTurnClass).slideDown('fast');
+        $(myTurnClass).css('display', 'none');
+      }, 5000)
+    }
+    if (turnCounter % 2 === 0){
+      switchTurn('.shaka-turn', '.bro-turn');
+    } else {
+      switchTurn('.bro-turn', '.shaka-turn');
+    }
+  }
+
+  function stopTimer(){
+    clearTimeout(timer);
+  }
+
+  function computerTurn(){
+    var randomClick = Math.floor(Math.random() * 9);
+    var grid = $('.grid');
+    if ($(grid[randomClick]).hasClass('clicked')){
+      computerTurn();
+    } else {
+      myTurnBro(bro, broMoves, '.bro', 'shaka-turn', 'bro-turn', $(grid[randomClick]));
+    }
+  }
+
+  switchTurnsCountdown();
+
   $('.grid').on('click', function(){
     
     //Prevents double clicks
@@ -58,12 +93,20 @@ $(document).ready(function(){
     //Shaka turn
     if (turnCounter % 2 === 0){
       var that = this;
-      myTurnBro(shaka, shakaMoves, shakaScore, '.shaka', that);
+      myTurnBro(shaka, shakaMoves, '.shaka', '.shaka-turn', '.bro-turn', that);
+      console.log(turnCounter);
+      if(playingAgainstComputer){
+        setTimeout(function(){
+          computerTurn();
+          turnCounter -= 1;
+          return 'nothing';
+        }, 5000);
+      }
 
     //Bro turn
     } else {
       var that = this;
-      myTurnBro(bro, broMoves, broScore, '.bro', that);
+      myTurnBro(bro, broMoves, '.bro', '.bro-turn', '.shaka-turn', that);
     }
 
     //Need to add modal
